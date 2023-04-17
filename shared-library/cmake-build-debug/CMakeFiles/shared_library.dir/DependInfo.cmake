04090fea9bb930a702980ee1d052c9c66b51865a
Idan <idan.sm@icloud.com>
2023-04-17 15:00:34 +0000

    added files

---
+++ shared-library/cmake-build-debug/CMakeFiles/shared_library.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/idan-ubuntu/CLionProjects/linux-assignment/shared-library/Database.cpp" "CMakeFiles/shared_library.dir/Database.cpp.o" "gcc" "CMakeFiles/shared_library.dir/Database.cpp.o.d"
+  "/home/idan-ubuntu/CLionProjects/linux-assignment/shared-library/Flight.cpp" "CMakeFiles/shared_library.dir/Flight.cpp.o" "gcc" "CMakeFiles/shared_library.dir/Flight.cpp.o.d"
+  "/home/idan-ubuntu/CLionProjects/linux-assignment/shared-library/main.cpp" "CMakeFiles/shared_library.dir/main.cpp.o" "gcc" "CMakeFiles/shared_library.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ shared-library/cmake-build-debug/CMakeFiles/shared_library.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_library.dir/Database.cpp.o"
+  "CMakeFiles/shared_library.dir/Database.cpp.o.d"
+  "CMakeFiles/shared_library.dir/Flight.cpp.o"
+  "CMakeFiles/shared_library.dir/Flight.cpp.o.d"
+  "CMakeFiles/shared_library.dir/main.cpp.o"
+  "CMakeFiles/shared_library.dir/main.cpp.o.d"
+  "shared_library"
+  "shared_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
