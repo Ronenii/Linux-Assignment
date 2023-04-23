fa61454f37998509d1a655134a34bcb21965ccd2
Ronenii <ronen.glm@gmail.com>
2023-04-23 20:40:13 +0300

    Started work on prog1

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Flight.h"
+#include "AirportFlights.h"
+#include "Functions.h"
 
-int main() {
+int main(int args, char*argv[]) {
     std::cout << "Hello, World!" << std::endl;
+    vector<AirportFlights> DB = AirportFlights::getDatabase();
     return 0;
 }
