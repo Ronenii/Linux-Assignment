88f06982d20281d92d27796f7ea59d687f37438d
Idan <idan.sm@icloud.com>
2023-04-23 17:25:49 +0000

    finished program 2

---
+++ program2/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include "Flight.h"
+#include "Functions.h"
+#include "AirportFlights.h"
+
+/**
+ * Compare two flights by the time of arrival/departure to/from a given airport.
+ */
+bool compareFlights(const Flight &f1, const Flight &f2, const string &airport_name) {
+    int time_f1, time_f2;
+
+    if(f1.getEstArrivalAirport() == airport_name)
+        time_f1 = f1.getLastSeen();
+    else
+        time_f1 = f1.getFirstSeen();
+
+    if(f2.getEstArrivalAirport() == airport_name)
+        time_f2 = f2.getLastSeen();
+    else
+        time_f2 = f2.getFirstSeen();
+
+    return time_f1 < time_f2;
+}
+
+/**
+ * Get the time of arrival/departure to/from a given airport.
+ */
+int getTime(const Flight &f, const string &airport_name) {
+    if(f.getEstArrivalAirport() == airport_name)
+        return f.getLastSeen();
+    else
+        return f.getFirstSeen();
+}
+
+bool isArriving(const Flight &f, const string &airport_name) {
+    return f.getEstArrivalAirport() == airport_name;
+}
+
+int main(int argc, char *argv[]) {
+    string airport_name = argv[1];
+
+    // Get the database from the files:
+    vector<AirportFlights> database = AirportFlights::getDatabase();
+
+    // Get all flights of received airport:
+    vector<Flight> airport_flights = Functions::getFlightsByAirportName(airport_name, database);
+
+    // Bubble sort the vector using the compareFlights function:
+    for (int i = 0; i < airport_flights.size() - 1; ++i) {
+        for (int j = i + 1; j < airport_flights.size(); ++j) {
+            if(!compareFlights(airport_flights[i], airport_flights[j], airport_name))
+                swap(airport_flights[i], airport_flights[j]);
+        }
+    }
+
+    // Print sorted flights details:
+    for(const Flight& curr : airport_flights) {
+        if(isArriving(curr, airport_name))  // Arriving to received airport.
+            cout << "Flight " << curr.getCallsign() << " arriving from " << curr.getEstDepartureAirport() << " at " << getTime(curr, airport_name) << endl;
+        else                                   // Departing from received airport.
+            cout << "Flight " << curr.getCallsign() << " departing to " << curr.getEstArrivalAirport() << " at " << getTime(curr, airport_name) << endl;
+    }
+    return 0;
+}
