1f26dc8711c7a4734e02409a213a887237ab8437
Idan <idan.sm@icloud.com>
2023-04-26 09:51:41 +0000

    fixed issue.

--- compile.sh
+++ compile.sh
@@ -8,10 +8,18 @@ g++ -c -fPIC src/program2.cpp -o objects/program2.o
 g++ -c -fPIC src/program3.cpp -o objects/program3.o
 g++ -c -fPIC src/program4.cpp -o objects/program4.o
 
+g++ -shared -fPIC -o objects/libshared_library.so objects/Functions.o objects/AirportFlights.o objects/Flight.o
 
-g++ -shared -fPIC -o objects/libshared_library.so objects/Functions.o objects/AirportFlights.o objects/Flight.o 
+g++ src/program1.cpp -o program1.out -I./lib -L. -lshared_library
+g++ src/program2.cpp -o program2.out -I./lib -L. -lshared_library
+g++ src/program3.cpp -o program3.out -I./lib -L. -lshared_library
+g++ src/program4.cpp -o program4.out -I./lib -L. -lshared_library
 
+# add current directory to the library path- where it will search for external libraries.
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD"
+export LD_LIBRARY_PATH
 
-g++ src/program1.cpp -o program1.out -I./lib -L./objects -lshared_library
-g++ src/program3.cpp -o program3.out -I./lib -L./objects -lshared_library
-
+# run the program from here. you can't run the program from outside the script because the LD_LIBRARY PATH
+# is only set for the current program and not outside of it. it is possible to run the above command
+# without the export to use it outside of this script's scope.
+./program1.out
